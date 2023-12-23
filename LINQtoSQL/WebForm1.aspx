46cf9f0e73b91ff4ae1dd1a566fff5466a7b0f1b
durgaprasad226 <durgaprasadmalthumkar@gmail.com>
2023-12-23 14:16:11 +0530

    MyCommit

---
+++ LINQtoSQL/WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="LINQtoSQL.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:GridView ID="GridView1" runat="server" ></asp:GridView>
+
+            <br />
+            <asp:Label ID="lblDept" runat="server"></asp:Label>
+
+            <br />
+            <br />
+            <asp:Button ID="btnGetData" runat="server" OnClick="btnGetData_Click" Text="GetData" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnInsert" runat="server" OnClick="btnInsert_Click" Text="InsertData" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="UpdateData" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" Text="DeleteData" />
+            <br />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnGetEmployeeByDept" runat="server" OnClick="btnGetEmployeeByDept_Click" Text="Get Employee By Department" />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ LINQtoSQL/WebForm1.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINQtoSQL
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+         /*private void GetData()
+         {
+            SampleDataContext sdc = new SampleDataContext();
+            //sdc.Log = Response.Output;
+            //sdc.Log = Console.Out;
+            var linqQuery = from employee in sdc.Employees
+                            select employee;
+            *//*string sqlQuery= linqQuery.ToString();
+            Response.Write(sqlQuery);*//*
+            Response.Write(sdc.GetCommand(linqQuery).CommandText);
+            GridView1.DataSource = linqQuery;
+            GridView1.DataBind();
+         }*/
+        private void GetData()
+        {
+            SampleDataContext sdc=new SampleDataContext();
+            GridView1.DataSource=sdc.GetEmployees();
+            GridView1.DataBind();
+        }
+
+        protected void btnGetData_Click(object sender, EventArgs e)
+        {
+            GetData();
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            using(SampleDataContext sdc= new SampleDataContext())
+            {
+                Employee emp = new Employee
+                {
+                    EmpName = "Mahesh",
+                    Gender="Male",
+                    Salary=3000,
+                    DeptId=2
+                };
+                sdc.Employees.InsertOnSubmit(emp);
+                sdc.SubmitChanges();
+            }
+            GetData();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using(var sdc=new  SampleDataContext())
+            {
+                Employee emp=sdc.Employees.FirstOrDefault(x=>x.EmpId==1);
+                emp.Salary = 30000;
+                sdc.SubmitChanges();
+            }
+            GetData();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            using(var sdc=new SampleDataContext())
+            {
+                Employee emp=sdc.Employees.FirstOrDefault(x=>x.EmpId == 7);
+                sdc.Employees.DeleteOnSubmit(emp);
+                sdc.SubmitChanges();
+            }
+            GetData();
+        }
+
+        protected void btnGetEmployeeByDept_Click(object sender, EventArgs e)
+        {
+            using(SampleDataContext sdc=new SampleDataContext())
+            {
+                string deptName=string.Empty;
+                GridView1.DataSource = sdc.GetEmployeesByDept(3, ref deptName);
+                GridView1.DataBind();
+
+                lblDept.Text="Department Name = " + deptName;
+            }
+        }
+    }
+}
